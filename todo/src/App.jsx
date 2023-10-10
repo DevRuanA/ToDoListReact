e7a330943ca6edcf63b0556ca9595efc606949d6
Ruan Almeida <ruanalmeida134@gmail.com>
2023-10-10 19:01:43 -0300

    App.jsx Id
    Adicionando id

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,6 +1,28 @@
 import { useState } from 'react'
 function App() {
-  return (
+  const[todos, setTodos] = useState ([
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+
+return (
+
 <div>Olá React</div>
   )
 }
